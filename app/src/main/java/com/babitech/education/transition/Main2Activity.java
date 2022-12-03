3d13fd6f66864ff68be7152d3a28494db87f241c
Babi Mumba <babimumba243@gmail.com>
2022-12-04 00:59:12 +0200

    add ewn file

---
+++ app/src/main/java/com/babitech/education/transition/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.babitech.education.transition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.babitech.education.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sha2);
+    }
+}
---
+++ app/src/main/java/com/babitech/education/transition/MainActivity.java
@@ -0,0 +1,48 @@
+package com.babitech.education.transition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.babitech.education.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity
+{
+    CircleImageView iv2;
+    TextView name;
+    LinearLayout lin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sha1);
+
+        iv2 = findViewById(R.id.iv2);
+        name = findViewById(R.id.name);
+        lin = findViewById(R.id.lin);
+
+        lin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharedIntent = new Intent(MainActivity.this, Main2Activity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(iv2, "imageTransition");
+                pairs[1] = new Pair<View, String>(name, "nameTransition");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+
+                startActivity(sharedIntent, options.toBundle());
+            }
+        });
+    }
+}
