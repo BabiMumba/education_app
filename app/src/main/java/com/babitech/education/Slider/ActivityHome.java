37ab9f7b7aefb9c97f823badb959cf01a1667ac1
Babi Mumba <babimumba243@gmail.com>
2022-12-03 23:28:31 +0200

    add enw file

---
+++ app/src/main/java/com/babitech/education/Slider/ActivityHome.java
@@ -0,0 +1,16 @@
+package com.babitech.education.Slider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.babitech.education.R;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/babitech/education/fragment/CoursFragment.kt
+++ app/src/main/java/com/babitech/education/fragment/CoursFragment.kt
@@ -35,7 +35,6 @@ class CoursFragment : Fragment() {
             
         }
 
-
         return v
     }
 
